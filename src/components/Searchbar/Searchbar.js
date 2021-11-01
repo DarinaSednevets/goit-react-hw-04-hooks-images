fd97a95d1b27e1d9b3a334a5a681989c3c5f3ff2
DarinaSednevets <dariaausten@gmail.com>
2021-11-01 12:26:31 +0200

    changed searchBar on hooks

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,45 +1,40 @@
-import { Component } from "react";
+
+import { useState } from "react";
 import styles from "./Searchbar.module.css"
 import PropTypes from "prop-types"
 
-class Searchbar extends Component {
+function Searchbar(props) {
+    const [query, setQuery] = useState('');
 
-    state = {
-        query: ''
-    }
-    handleChange = event => {
-        this.setState({
-            query: event.currentTarget.value,
-        })
-    }
+    const handleChange = event => {
+        setQuery(event.target.value);
+    };
 
-    handleSubmit = event => {
+
+    const handleSubmit = event => {
         event.preventDefault();
-        this.props.onSubmit(this.state.query);
-        this.setState({ query: '' });
+        props.onSubmit(query);
+        setQuery('');
     }
 
+    return (
+        <header className={styles.Searchbar}>
+            <form onSubmit={handleSubmit} className={styles.SearchForm}>
+                <button type="submit" className={styles.Button}>
+                    <span className={styles.ButtonLabel}>Search</span>
+                </button>
 
-    render() {
-        return (
-            <header className={styles.Searchbar}>
-                <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
-                    <button type="submit" className={styles.Button}>
-                        <span className={styles.ButtonLabel}>Search</span>
-                    </button>
-
-                    <input
-                        className={styles.Input}
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        onChange={this.handleChange}
-                    />
-                </form>
-            </header>
-        )
-    }
+                <input
+                    className={styles.Input}
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    onChange={handleChange}
+                />
+            </form>
+        </header>
+    )
 }
 
 export default Searchbar;
